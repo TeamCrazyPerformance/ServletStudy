82bf2a6d72f352644279120410ebadb782390d49
oeno <soye73@naver.com>
2016-12-03 19:56:47 +0900

    [Songyeol] Add codility problem

--- songyeol/codility/src/Arrays/CyclicRotation.java
+++ songyeol/codility/src/Arrays/CyclicRotation.java
@@ -3,5 +3,18 @@ package Arrays;
 /**
  * Created by Sonkrat on 2016. 11. 20..
  */
-public class CyclicRotation {
+
+class CycleRotation {
+    public int[] solution(int[] A, int K) {
+        // write your code in Java SE 8
+        int size = A.length;
+        int [] result = new int[size];
+        int idx = K;
+
+        for (int n : A) {
+            result[(idx++)%size] = n;
+        }
+
+        return result;
+    }
 }
--- songyeol/codility/src/Arrays/OddOccurrencesInArray.java
+++ songyeol/codility/src/Arrays/OddOccurrencesInArray.java
@@ -1,7 +1,36 @@
 package Arrays;
 
+import java.util.Stack;
+
 /**
  * Created by Sonkrat on 2016. 11. 20..
  */
 public class OddOccurrencesInArray {
+//    public int solution(int[] A) {
+//        // write your code in Java SE 8
+//        int result = 0;
+//        int size = A.length;
+//        int[] temp = new int[size];
+//
+//        for (int i = 0; i < size; i++) {
+//            result += A[i];
+//            for (int j = 0; j < i; j++) {
+//                if (A[i] == A[j]) {
+//                    result -= A[i] * 2;
+//                    A[i] = A[j] = 0;
+//                }
+//            }
+//        }
+//        return result;
+//    }
+
+    // 결국 찾아봤지만.. 어떻게 이런생각을....?
+    public int solution(int[] A) {
+        int odd = 0;
+        for (int n : A) {
+            odd ^= n;
+        }
+
+        return odd;
+    }
 }
